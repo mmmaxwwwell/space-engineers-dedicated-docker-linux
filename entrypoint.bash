5581ff19eca2a137d5d3afceb1467afdf1fd77be
mmmaxwwwell <mmmaxwwwell@massive.games>
2020-10-01 18:41:10 -0400

    Devidian's contribution

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
-/usr/games/steamcmd +login anonymous +force_install_dir /appdata/space-engineers/SpaceEngineersDedicated +app_update 298740 +quit || exit 0 
+/usr/games/steamcmd +login anonymous +@sSteamCmdForcePlatformType windows +force_install_dir /appdata/space-engineers/SpaceEngineersDedicated +app_update 298740 +quit
+chown -R space_engineers /appdata
 runuser space_engineers -c /entrypoint-space_engineers.bash
