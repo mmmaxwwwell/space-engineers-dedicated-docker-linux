ba6e75a96b4c1827512a2a53c9255151bcc48e7e
mmmaxwwwell <amclauss1@gmail.com>
2020-10-01 18:41:10 -0400

    Devidian's contribution

--- entrypoint-space_engineers.bash
+++ entrypoint-space_engineers.bash
@@ -2,4 +2,4 @@
 source ~/.profile
 source ~/.bash_profile
 cd /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
-env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/home/space_engineers/server wine /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/SpaceEngineersDedicated.exe -console -path Z:\\appdata\\space-engineers\\SpaceEngineersDedicated -ignorelastsession
+env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/home/space_engineers/server wine /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/SpaceEngineersDedicated.exe -noconsole -path Z:\\appdata\\space-engineers\\SpaceEngineersDedicated -ignorelastsession
