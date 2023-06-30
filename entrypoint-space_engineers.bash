6a8997fee9dcbe557fccda3f1619231fcd1ac44e
mmmaxwwwell <mmmaxwwwell@protonmail.com>
2023-06-30 00:54:30 -0400

    something went wrong with steam

--- entrypoint-space_engineers.bash
+++ entrypoint-space_engineers.bash
@@ -2,6 +2,6 @@
 source ~/.profile
 source ~/.bash_profile
 cd /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
-echo "Starting Server.."
+echo "Starting Server."
 env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/wineprefix wine /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/SpaceEngineersDedicated.exe -noconsole -path Z:\\appdata\\space-engineers\\SpaceEngineersDedicated -ignorelastsession
 echo "Exiting Server Process.."
--- entrypoint.bash
+++ entrypoint.bash
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 # #check if /appdata/space-engineers/config/World is a folder
-if [ ! -d "/appdata/space-engineers/World" ]; then
+if [ ! -d "/appdata/space-engineers/World/World" ]; then
   echo "World folder does not exist, exiting"
   exit 129
 fi
 
-# #check if /appdata/space-engineers/config/World/Sandbox.sbc exists and is a file
-if [ ! -f "/appdata/space-engineers/World/Sandbox.sbc" ]; then
-  echo "Sandbox.sbc file does not exist, exiting."
-  exit 130
-fi
+# # #check if /appdata/space-engineers/config/World/Sandbox.sbc exists and is a file
+# if [ ! -f "/appdata/space-engineers/World/World/Sandbox.sbc" ]; then
+#   echo "Sandbox.sbc file does not exist, exiting."
+#   exit 130
+# fi
 
 # #check if /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg is a file
-if [ ! -f "/appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg" ]; then
+if [ ! -f "/appdata/space-engineers/World/SpaceEngineers-Dedicated.cfg" ]; then
   echo "SpaceEngineers-Dedicated.cfg file does not exist, exiting."
   exit 131
+else
+  rm -rf /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+  cp /appdata/space-engineers/World/SpaceEngineers-Dedicated.cfg /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
 fi
 
 #set <LoadWorld> to the correct value
-cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E '/.*LoadWorld.*/c\  <LoadWorld>Z:\\appdata\\space-engineers\\World</LoadWorld>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E '/.*LoadWorld.*/c\  <LoadWorld>Z:\\appdata\\space-engineers\\World\\World</LoadWorld>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
 
 #set game port to the correct value
 #cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E '/.*ServerPort.*/c\  <ServerPort>27016</ServerPort>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
