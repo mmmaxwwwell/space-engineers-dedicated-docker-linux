96b0d5a78b197045c0724be4141a63e502884063
mmmaxwwwell <mmmaxwwwell@protonmail.com>
2023-07-04 16:53:40 -0400

    cleanup

--- entrypoint-space_engineers.bash
+++ entrypoint-space_engineers.bash
@@ -2,6 +2,4 @@
 source ~/.profile
 source ~/.bash_profile
 cd /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
-echo "Starting Server."
 env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/wineprefix wine /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/SpaceEngineersDedicated.exe -noconsole -path Z:\\appdata\\space-engineers\\SpaceEngineersDedicated -ignorelastsession
-echo "Exiting Server Process.."
